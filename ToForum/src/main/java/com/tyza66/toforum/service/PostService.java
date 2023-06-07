4bd5c9ce36121804a2552f5924cc215add12bd08
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-07 08:21:39 +0800

    改了一点Service层的逻辑

--- ToForum/src/main/java/com/tyza66/toforum/service/PostService.java
+++ ToForum/src/main/java/com/tyza66/toforum/service/PostService.java
@@ -1,5 +1,6 @@
 package com.tyza66.toforum.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tyza66.toforum.mapper.PostMapper;
@@ -24,19 +25,69 @@ public class PostService {
     @Resource
     private MongoTemplate mongoTemplate;
 
+    //分页查询最新的帖子
     public IPage<Post> getByPage(int page, int size) {
-        IPage<Post> postPage = postMapper.selectPage(new Page<>(page, size), null);
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("last");
+        IPage<Post> postPage = postMapper.selectPage(new Page<>(page, size), queryWrapper);
         return postPage;
     }
 
-    public Boolean savePost(PostStract post, String collectionName){
-        mongoTemplate.save(post,collectionName);
+    //分页查询最新发布的的帖子
+    public IPage<Post> getNewByPage(int page, int size) {
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("first");
+        IPage<Post> postPage = postMapper.selectPage(new Page<>(page, size), queryWrapper);
+        return postPage;
+    }
+
+    //分页查询管理员的帖子
+    public IPage<Post> getAdminByPage(int page, int size) {
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("owner", "admin");
+        queryWrapper.orderByDesc("last");
+        IPage<Post> postPage = postMapper.selectPage(new Page<>(page, size), queryWrapper);
+        return postPage;
+    }
+
+    //保存帖子体
+    public Boolean savePost(PostStract post, String collectionName) {
+        mongoTemplate.save(post, collectionName);
         return true;
     }
 
-    public List<PostStract> selectPostStract(String collectionName){
+    //查询帖子体集合
+    public List<PostStract> selectPostStract(String collectionName) {
         List<PostStract> postStracts = mongoTemplate.findAll(PostStract.class, collectionName);
         return postStracts;
+    }
+
+    //发布新帖子
+    public Boolean send(Post post, PostStract postStract) {
+        int insert = postMapper.insert(post);
+        mongoTemplate.save(postStract, "post" + post.getId());
+        if (insert >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //根据id查询帖子
+    public Post getById(int id) {
+        Post post = postMapper.selectById(id);
+        return post;
+    }
+
+    //根据id删除帖子
+    public Boolean deleteById(int id) {
+        int i = postMapper.deleteById(id);
+        mongoTemplate.dropCollection("post" + id);
+        if (i >= 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
