1a5cb2fec73a1646e193c2e7c9ccccc304ede7d7
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-08 14:33:09 +0800

    浅更新一下数据库表

--- 数据库表/mongo/totan.sql
+++ 数据库表/mongo/totan.sql
@@ -11,9 +11,76 @@
  Target Server Version : 40422
  File Encoding         : 65001
 
- Date: 07/06/2023 19:20:16
+ Date: 08/06/2023 14:21:51
 */
 
+
+// ----------------------------
+// Collection structure for 1
+// ----------------------------
+db.getCollection("1").drop();
+db.createCollection("1");
+
+// ----------------------------
+// Documents of 1
+// ----------------------------
+db.getCollection("1").insert([ {
+    _id: "f8e74d9ab9654763bc9ed23e5d647247",
+    owner: "admin",
+    last: "2023-06-08 12:46:19.802",
+    first: "2023-06-08 12:46:19.802",
+    in: "2",
+    collectionName: "1",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for 166641b2787f4cf49b8665bf3dce8482
+// ----------------------------
+db.getCollection("166641b2787f4cf49b8665bf3dce8482").drop();
+db.createCollection("166641b2787f4cf49b8665bf3dce8482");
+
+// ----------------------------
+// Documents of 166641b2787f4cf49b8665bf3dce8482
+// ----------------------------
+db.getCollection("166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "5dc943a732f94a3e840fd5b0d4559f35",
+    owner: "tyza66",
+    last: "2023-06-08 12:53:40.512",
+    first: "2023-06-08 12:53:40.512",
+    in: "# 请在此处输入回复内容",
+    collectionName: "166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "7a03ea66889e44758fe960ce7006aea4",
+    owner: "tyza66",
+    last: "2023-06-08 12:53:46.498",
+    first: "2023-06-08 12:53:46.498",
+    in: "# 请在此处输入回复内容",
+    collectionName: "166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for post048852e784244d409cba0687cf89955c
+// ----------------------------
+db.getCollection("post048852e784244d409cba0687cf89955c").drop();
+db.createCollection("post048852e784244d409cba0687cf89955c");
+
+// ----------------------------
+// Documents of post048852e784244d409cba0687cf89955c
+// ----------------------------
+db.getCollection("post048852e784244d409cba0687cf89955c").insert([ {
+    _id: ObjectId("64812a2ca63891716b2a5567"),
+    title: "giao",
+    owner: "tyza66",
+    last: ISODate("2023-06-08T01:09:00.229Z"),
+    first: ISODate("2023-06-08T01:09:00.229Z"),
+    tags: "giaogiao",
+    in: "#                              在这里编辑你的帖子正文",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
 
 // ----------------------------
 // Collection structure for post1
@@ -35,5 +102,388 @@ db.getCollection("post1").insert([ {
     tags: "",
     in: "",
     collectionName: "post1",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post1").insert([ {
+    _id: "bc2ea884f26841a8b439d59a242ef556",
+    owner: "admin",
+    last: "2023-06-08 12:46:42.035",
+    first: "2023-06-08 12:46:42.035",
+    in: "2",
+    collectionName: "post1",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for post166641b2787f4cf49b8665bf3dce8482
+// ----------------------------
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").drop();
+db.createCollection("post166641b2787f4cf49b8665bf3dce8482");
+
+// ----------------------------
+// Documents of post166641b2787f4cf49b8665bf3dce8482
+// ----------------------------
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: ObjectId("64814a48d72a7336798906ec"),
+    title: "大范围让她分手",
+    owner: "tyza66",
+    last: "2023-06-08 11:26:00.586",
+    first: "2023-06-08 11:26:00.586",
+    tags: "发生大法师发",
+    in: "#                              在这里编辑你的帖子正文\\\\n\\n\\nfdssdfgsdgsdfgsdfg\\n\\n\\n\\ndfsgsdgszg\\n\\n\\nzsegawgt",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "ed748484bf7f40f8a0af890b097a327c",
+    owner: "tyza66",
+    last: "2023-06-08 12:56:52.439",
+    first: "2023-06-08 12:56:52.439",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "5b15786e5dba4d79a83b9b08b09c6536",
+    owner: "tyza66",
+    last: "2023-06-08 12:57:01.228",
+    first: "2023-06-08 12:57:01.228",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "e7904d3bcfe141c0b6c9d8ea0c0537c4",
+    owner: "tyza66",
+    last: "2023-06-08 12:57:55.489",
+    first: "2023-06-08 12:57:55.489",
+    in: "# 请在此处输入回复内容\\nsdfgsdfgs sg",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "32f19a74d84c46abaf5b1a0a3aff25a9",
+    owner: "tyza66",
+    last: "2023-06-08 12:58:05.13",
+    first: "2023-06-08 12:58:05.13",
+    in: "# 请在此处输入回复内容\\n你好",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "bbcd69695b8847148f421410942a42bb",
+    owner: "tyza66",
+    last: "2023-06-08 12:58:11.995",
+    first: "2023-06-08 12:58:11.995",
+    in: "# 请在此处输入回复内容\\n\\n### 不该",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "f589770cbbd34469b7eb2b93568c1e3e",
+    owner: "tyza66",
+    last: "2023-06-08 13:15:01.941",
+    first: "2023-06-08 13:15:01.941",
+    in: "# 请在此处输入回复内容\\n\\nsafafasfffsdfdsfdsfdfsdfsdf",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "76676bc554a64fd9873c66334f248df0",
+    owner: "tyza66",
+    last: "2023-06-08 13:33:29.065",
+    first: "2023-06-08 13:33:29.065",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "aed73fd966ee45b18d367a699979e33a",
+    owner: "tyza66",
+    last: "2023-06-08 13:33:32.61",
+    first: "2023-06-08 13:33:32.61",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "28ddf0ab6a884dbeb936a4a83da6791d",
+    owner: "tyza66",
+    last: "2023-06-08 13:37:28.204",
+    first: "2023-06-08 13:37:28.204",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "4ccce46c93c14580ada7649e29fb5165",
+    owner: "tyza66",
+    last: "2023-06-08 13:37:34.131",
+    first: "2023-06-08 13:37:34.131",
+    in: "# 请在此处输入回复内容\\\\nasdfadf ",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "66fb40626dae43ab92d9e26e6b370658",
+    owner: "tyza66",
+    last: "2023-06-08 13:37:36.644",
+    first: "2023-06-08 13:37:36.644",
+    in: "# 请在此处输入回复内容\\\\nasdfadf ",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "a9430767618b4aa680898239d02f9526",
+    owner: "tyza66",
+    last: "2023-06-08 13:37:37.072",
+    first: "2023-06-08 13:37:37.072",
+    in: "# 请在此处输入回复内容\\\\nasdfadf ",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "a4b06ac894f74f78935a0f1be00f3354",
+    owner: "tyza66",
+    last: "2023-06-08 13:37:37.259",
+    first: "2023-06-08 13:37:37.259",
+    in: "# 请在此处输入回复内容\\\\nasdfadf ",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "bb40520ef9054db2a085fd31ed1d166a",
+    owner: "tyza66",
+    last: "2023-06-08 13:38:25.97",
+    first: "2023-06-08 13:38:25.97",
+    in: "# 请在此处输入回复内容\\nadaQfwqeatA3WTFRAwtWEATSATG",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "bbc93d0451ff440d84066f56f974c69e",
+    owner: "tyza66",
+    last: "2023-06-08 13:38:31.328",
+    first: "2023-06-08 13:38:31.328",
+    in: "# 请在此处输入回复内容\\nadaQfwqeatA3WTFRAwtWEATSATG",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "e505ebb389f842628a8bbad8f7147b0c",
+    owner: "tyza66",
+    last: "2023-06-08 13:38:32.068",
+    first: "2023-06-08 13:38:32.068",
+    in: "# 请在此处输入回复内容\\nadaQfwqeatA3WTFRAwtWEATSATG",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "1f23cf64a39142608140b53197f4da7a",
+    owner: "tyza66",
+    last: "2023-06-08 13:38:37.489",
+    first: "2023-06-08 13:38:37.489",
+    in: "# 请在此处输入回复内容\\nadaQfwqeatA3WTFRAwtWEATSATG",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "c43ba15162de4dd3a24cdc94b2fcc4c1",
+    owner: "tyza66",
+    last: "2023-06-08 13:38:40.311",
+    first: "2023-06-08 13:38:40.311",
+    in: "# 请在此处输入回复内容\\nadaQfwqeatA3WTFRAwtWEATSATG",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "23497f69ba7e46e08856643318353712",
+    owner: "tyza66",
+    last: "2023-06-08 13:38:40.802",
+    first: "2023-06-08 13:38:40.802",
+    in: "# 请在此处输入回复内容\\nadaQfwqeatA3WTFRAwtWEATSATG",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "dceb1ae6e5ca44c1a4fb16b6b0f6c89a",
+    owner: "tyza66",
+    last: "2023-06-08 13:40:33.922",
+    first: "2023-06-08 13:40:33.922",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "f70e4bc917314643bade4a19de6ed139",
+    owner: "tyza66",
+    last: "2023-06-08 13:40:35.62",
+    first: "2023-06-08 13:40:35.62",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "3bf8b3ada7d64ecc9a30cb4ca83f343e",
+    owner: "tyza66",
+    last: "2023-06-08 13:40:38.703",
+    first: "2023-06-08 13:40:38.703",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "b940ca551f3d46fbb0ae52b66f1d5401",
+    owner: "tyza66",
+    last: "2023-06-08 13:41:06.791",
+    first: "2023-06-08 13:41:06.791",
+    in: "# 请在此处输入回复内容",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "375e5ebda1834efcaee8ccb51801e38f",
+    owner: "tyza66",
+    last: "2023-06-08 13:41:47.567",
+    first: "2023-06-08 13:41:47.567",
+    in: "# 绝了\\n这个终于好了 也能换行了",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "fc1ca28b8bdf426bb325772e675e4216",
+    owner: "tyza66",
+    last: "2023-06-08 14:05:03.509",
+    first: "2023-06-08 14:05:03.509",
+    in: "# 请在此处输入回复内容a\\n'\\nfd\\nasdf\\nasfdsadfdfsd \\ngf\\ng\\ndfgdfs\\ngsd\\ngfd\\nghdf\\nghdf\\nhgf\\nhjfgjhgkjtrjdc\\njr\\nxdj\\nrszh\\neaz\\n\\n\\nasfsafsdgsdg",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+db.getCollection("post166641b2787f4cf49b8665bf3dce8482").insert([ {
+    _id: "faf1de23b1c0482d9662fc77a13d9c2a",
+    owner: "tyza66",
+    last: "2023-06-08 14:16:21.593",
+    first: "2023-06-08 14:16:21.593",
+    in: "# 请在此处输入回复内容\\ndsajdfhioa\\nfgsdgehsyer\\n\\n~~## 给i奥~~\\n",
+    collectionName: "post166641b2787f4cf49b8665bf3dce8482",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for post9132e70ef5f0430fa3b1907f19ce55b3
+// ----------------------------
+db.getCollection("post9132e70ef5f0430fa3b1907f19ce55b3").drop();
+db.createCollection("post9132e70ef5f0430fa3b1907f19ce55b3");
+
+// ----------------------------
+// Documents of post9132e70ef5f0430fa3b1907f19ce55b3
+// ----------------------------
+db.getCollection("post9132e70ef5f0430fa3b1907f19ce55b3").insert([ {
+    _id: ObjectId("648122f057fa872a580bdbe1"),
+    title: "1",
+    owner: "admin",
+    last: ISODate("2023-06-08T00:38:08.532Z"),
+    first: ISODate("2023-06-08T00:38:08.532Z"),
+    tags: "2",
+    in: "3",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for posta2edbe92eadf4be89d9e44a0d93cec7f
+// ----------------------------
+db.getCollection("posta2edbe92eadf4be89d9e44a0d93cec7f").drop();
+db.createCollection("posta2edbe92eadf4be89d9e44a0d93cec7f");
+
+// ----------------------------
+// Documents of posta2edbe92eadf4be89d9e44a0d93cec7f
+// ----------------------------
+db.getCollection("posta2edbe92eadf4be89d9e44a0d93cec7f").insert([ {
+    _id: ObjectId("648149c0b0e4a52e0e2392d5"),
+    title: "jing",
+    owner: "tyza66",
+    last: "2023-06-08 11:23:44.771",
+    first: "2023-06-08 11:23:44.771",
+    tags: "#giao",
+    in: "#                              在这里编辑你的帖子正文\n是的哈岁的发货了\n\n\n发生发射点",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for postc2f4fce4d8124c4086affa0d67875f36
+// ----------------------------
+db.getCollection("postc2f4fce4d8124c4086affa0d67875f36").drop();
+db.createCollection("postc2f4fce4d8124c4086affa0d67875f36");
+
+// ----------------------------
+// Documents of postc2f4fce4d8124c4086affa0d67875f36
+// ----------------------------
+db.getCollection("postc2f4fce4d8124c4086affa0d67875f36").insert([ {
+    _id: ObjectId("648129e3a63891716b2a5566"),
+    title: "giao",
+    owner: "tyza66",
+    last: ISODate("2023-06-08T01:07:47.634Z"),
+    first: ISODate("2023-06-08T01:07:47.634Z"),
+    tags: "giaogiao",
+    in: "#                              在这里编辑你的帖子正文",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for postc428f773f9e84877a749c3fcf5287c34
+// ----------------------------
+db.getCollection("postc428f773f9e84877a749c3fcf5287c34").drop();
+db.createCollection("postc428f773f9e84877a749c3fcf5287c34");
+
+// ----------------------------
+// Documents of postc428f773f9e84877a749c3fcf5287c34
+// ----------------------------
+db.getCollection("postc428f773f9e84877a749c3fcf5287c34").insert([ {
+    _id: ObjectId("6481258b6f27e96d52eb05de"),
+    title: "家人们家人们",
+    owner: "tyza66",
+    last: ISODate("2023-06-08T00:49:14.991Z"),
+    first: ISODate("2023-06-08T00:49:14.991Z"),
+    tags: "家人们",
+    in: "#                              在这里编辑你的帖子正文",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for postd54a3e7556fc43fbb8a36835283d0abf
+// ----------------------------
+db.getCollection("postd54a3e7556fc43fbb8a36835283d0abf").drop();
+db.createCollection("postd54a3e7556fc43fbb8a36835283d0abf");
+
+// ----------------------------
+// Documents of postd54a3e7556fc43fbb8a36835283d0abf
+// ----------------------------
+db.getCollection("postd54a3e7556fc43fbb8a36835283d0abf").insert([ {
+    _id: ObjectId("64812df92cf3703906697716"),
+    title: "惹3情人桥wrsafdas",
+    owner: "tyza66",
+    last: ISODate("2023-06-08T01:25:13.447Z"),
+    first: ISODate("2023-06-08T01:25:13.447Z"),
+    tags: "飞洒飞洒地方士大夫我fresf",
+    in: "#                              在这里编辑你的帖子正文asdfadqa\nfsdsddggfdg\n\ngdsgsdgs\ngfdsgfdsfgdfsgdgsdgs\n\nsdgfsdgfsd",
+    _class: "com.tyza66.toforum.pojo.PostStract"
+} ]);
+
+// ----------------------------
+// Collection structure for postfc7de87740d546e780449c0557c89d7f
+// ----------------------------
+db.getCollection("postfc7de87740d546e780449c0557c89d7f").drop();
+db.createCollection("postfc7de87740d546e780449c0557c89d7f");
+
+// ----------------------------
+// Documents of postfc7de87740d546e780449c0557c89d7f
+// ----------------------------
+db.getCollection("postfc7de87740d546e780449c0557c89d7f").insert([ {
+    _id: ObjectId("648125a76f27e96d52eb05df"),
+    title: "666",
+    owner: "tyza66",
+    last: ISODate("2023-06-08T00:49:43.905Z"),
+    first: ISODate("2023-06-08T00:49:43.905Z"),
+    tags: "666",
+    in: "#                              在这里编辑你的帖子正文",
     _class: "com.tyza66.toforum.pojo.PostStract"
 } ]);
